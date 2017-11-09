8c42f3b4264aae833e50c4ad257fde4ea77634e8
Justin9four <justinchandler.17@gmail.com>
2017-11-08 20:42:39 -0700

    .

--- projects.js
+++ projects.js
@@ -83,7 +83,7 @@ function fetchData() {
 
 function writeData(responseText, id) {
     var myObj = JSON.parse(responseText);
-    var displayText = "";
+    var displayText = "<div class=\"flex-item\" id=\"item";
     var text = [];
     for (i=0; i<myObj.projects.length; i++) {
         var date = myObj.projects[i].hidden.date;
@@ -99,7 +99,13 @@ function writeData(responseText, id) {
         }
         var reference = myObj.projects[i].visible.links.reference;
         var ribbon = myObj.projects[i].visible.links.ribbon;
-        displayText += date + rank + type + title + projectInfo + primaryImg + description + reference + ribbon;
+        
+        displayText += rank + "\" style=\"order:\"" + Number(rank) + 1 + "\"> <h2>" + title + "</h2> <p>" + projectInfo + "</p> <div class=\"center\"> <img scr=\"" + primaryImg +  " alt=\"\" style=\"width:100%; height:auto\"> <a class=\"active\" id=\"expanded1Link\" onclick=\"expandedText('expanded1')\" style=\"color:blue;cursor:pointer;display:inline\"><u>More...</u></a></div><div id=\"id=\"expanded1\" style=\"display:none\"><div class=\"center\">";
+        for (k=0; k<secondaryImg.length; k++){
+            displayText += "<img src=\"" + secondaryImg[k] + "alt=\"\" style=\"width:100%; height:auto\">";
+        }  
+        displayText += "<a class=\"active\" href=\"" + reference + "\"> <button type=\"button\" style=\"font-size:25px\">" + ribbon + "</button> </a> </div> <div class=\"active\" onclick=\"expandedText('expanded1')\" style=\"color:blue;cursor:pointer\"><u>Less...</u></a> </div> </div> </div>"; 
+        //displayText += date + rank + type + title + projectInfo + primaryImg + description + reference + ribbon;
     }
     document.getElementById("projects").innerHTML = displayText;
 }
