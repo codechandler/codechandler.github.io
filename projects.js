4e26752c030ceda5b6cb2e7186ad4fcd01ab9a0e
Justin9four <justinchandler.17@gmail.com>
2017-11-10 17:15:36 -0700

    .

--- projects.js
+++ projects.js
@@ -101,11 +101,11 @@ function writeData(responseText, id) {
         var reference = myObj.projects[i].visible.links.reference;
         var ribbon = myObj.projects[i].visible.links.ribbon;
         
-        displayText += rank + "\" style=\"order:" + Number(rank) + 1 + "\"> <h2>" + title + "</h2> <p>" + projectInfo + "</p> <div class=\"center\"> <img src=\"" + primaryImg + "\" alt=\"\" style=\"width:100%; height:auto\"> <a class=\"active\" id=\"expanded1Link\" onclick=\"expandedText('expanded1')\" style=\"color:blue;cursor:pointer;display:inline\"><u>More...</u></a></div><div id=\"expanded1\" style=\"display:none\"><div class=\"center\">";
+        displayText += rank + "\" style=\"order:" + Number(rank) + 1 + "\"> <h2>" + title + "</h2> <p>" + projectInfo + "</p> <div class=\"center\"> <img src=\"" + primaryImg + "\" alt=\"\" style=\"width:100%; height:auto\"> <a class=\"active\" id=\"expanded" + Number(rank) + 1 + "Link\" onclick=\"expandedText('expanded" + Number(rank) + 1 + "\"')\" style=\"color:blue;cursor:pointer;display:inline\"><u>More...</u></a></div><div id=\"expanded" + Number(rank) + 1 + "\" style=\"display:none\"><div class=\"center\">";
         for (k=0; k<secondaryImg.length; k++){
             displayText += "<img src=\"" + secondaryImg[k] + "\" alt=\"\" style=\"width:100%; height:auto\">";
         }  
-        displayText += "<a class=\"active\" href=\"" + reference + "\"> <button type=\"button\" style=\"font-size:25px\">" + ribbon + "</button> </a> </div> <div class=\"active\" onclick=\"expandedText('expanded1')\" style=\"color:blue;cursor:pointer\"><u>Less...</u></a> </div> </div> </div>"; 
+        displayText += "<a class=\"active\" href=\"" + reference + "\"> <button type=\"button\" style=\"font-size:25px\">" + ribbon + "</button> </a> </div> <div class=\"active\" onclick=\"expandedText('expanded" + Number(rank) + 1 + "')\" style=\"color:blue;cursor:pointer\"><u>Less...</u></a> </div> </div> </div>"; 
         //displayText += date + rank + type + title + projectInfo + primaryImg + description + reference + ribbon;
     }
     alert(displayText);
