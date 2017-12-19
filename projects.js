3376144615a32a71b9760dedcd461b7a2599ee48
Justin9four <justinchandler.17@gmail.com>
2017-12-19 13:39:37 -0800

    into

--- projects.js
+++ projects.js
@@ -72,7 +72,7 @@ if (inputs[2].checked == true)
 
 function fetchData() {
     var id = "projects";
-    var url = "https://codechandler.github.io/projects.txt";
+    var url = "https://cors.io/?http://codechandler.com/projects.txt";
     httpRequest = false;
     httpRequest = new XMLHttpRequest();
     httpRequest.onreadystatechange = function() {
